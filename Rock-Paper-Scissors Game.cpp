f620bb879b5f80a0fe2ce0618afccbdae2d1db73
0xqben <nawar20062022.wow@gmail.com>
2026-07-04 19:57:20 +0300

    feat : create Random choice Function

--- Rock-Paper-Scissors Game.cpp
+++ Rock-Paper-Scissors Game.cpp
@@ -1,6 +1,7 @@
 // Rock-Paper-Scissors Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
+#include <cstdlib>
 
 enum enRockPaperScissors
 {
@@ -9,7 +10,26 @@ enum enRockPaperScissors
     Scissors=3,
 };
 
+int RandomNumber(int From, int To) {
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+enRockPaperScissors RandomChoice() {
+    int RandomChoice = RandomNumber(1, 3);
+    switch (RandomChoice) {
+    case 1 : 
+        return enRockPaperScissors::Rock;
+    case 2 : 
+        return enRockPaperScissors::Paper;
+    case 3 : 
+        return enRockPaperScissors::Scissors;
+    }
+}
+
 int main()
 {
-    
+    srand((unsigned)time(NULL));
+
+
 }
